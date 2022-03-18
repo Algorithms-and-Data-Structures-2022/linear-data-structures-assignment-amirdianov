b2732b7b22a38b4dec1efbd9beb740c8e1115df1
amirdianov <amirdianov@gmail.com>
2022-03-18 12:07:04 +0300

    dynamic_array

--- src/dynamic_array.cpp
+++ src/dynamic_array.cpp
@@ -30,17 +30,40 @@ namespace assignment {
   }
 
   void DynamicArray::Add(int value) {
-    // Write your code here ...
+    if (size_ == capacity_) {
+      capacity_ += kCapacityGrowthCoefficient;
+    }
+    size_ += 1;
+    data_[size_ - 1] = value;
   }
 
   bool DynamicArray::Insert(int index, int value) {
-    // Write your code here ...
-    return false;
+    if (0 <= index and index < size_){
+      if (size_ == capacity_){
+        capacity_ += kCapacityGrowthCoefficient;
+      }
+      size_ += 1;
+      for (int i = index + 1; i < size_; i++){
+        data_[i] = data_[i-1];
+      }
+      data_[index] = value;
+      return true;
+    }
+    else{
+      return false;
+    }
   }
 
   bool DynamicArray::Set(int index, int new_value) {
-    // Write your code here ...
-    return false;
+    if (size_ == 0) {
+      return false;
+    }
+    if (0 <= index and index < size_) {
+      data_[index] = new_value;
+      return true;
+    } else {
+      return false;
+    }
   }
 
   std::optional<int> DynamicArray::Remove(int index) {
@@ -53,18 +76,27 @@ namespace assignment {
   }
 
   std::optional<int> DynamicArray::Get(int index) const {
-    if (0 <= index and index < size_){
+    if (0 <= index and index < size_) {
       return data_[index];
     }
     return std::nullopt;
   }
 
   std::optional<int> DynamicArray::IndexOf(int value) const {
-    // Write your code here ...
+    for (int i = 0; i < size_; i++) {
+      if (data_[i] == value) {
+        return i;
+      }
+    }
     return std::nullopt;
   }
 
   bool DynamicArray::Contains(int value) const {
+    for (int i = 0; i < size_; i++) {
+      if (data_[i] == value) {
+        return true;
+      }
+    }
     return false;
   }
 
@@ -81,8 +113,12 @@ namespace assignment {
   }
 
   bool DynamicArray::Resize(int new_capacity) {
-    // Write your code here ...
-    return false;
+    if (new_capacity > capacity_) {
+      capacity_ = new_capacity;
+      return true;
+    } else {
+      return false;
+    }
   }
 
   // ДЛЯ ТЕСТИРОВАНИЯ
